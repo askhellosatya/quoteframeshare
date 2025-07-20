a97d06231151eead426dbc7d61c8329981b9c329
Bunty <bn.bhandari90@gmail.com>
2025-07-20 22:31:00 +0530

    Code Cleanup

--- quoteframeshare.php
+++ quoteframeshare.php
@@ -12,78 +12,19 @@
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-    exit;
+	exit;
 }
-
-/**
- * Registers the QuoteFrameShare block and its assets.
- */
-function quoteframeshare_register_block() {
-    $dir = plugin_dir_path( __FILE__ );
-    $asset_file = include( $dir . 'build/quote-frame-share/index.asset.php' );
-
-    // Register block editor script
-    wp_register_script(
-        'quoteframeshare-block',
-        plugins_url( 'build/index.js', __FILE__ ),
-        $asset_file['dependencies'],
-        $asset_file['version'],
-        true
-    );
-
-    // Register frontend script
-    wp_register_script(
-        'quoteframeshare-frontend',
-        plugins_url( 'build/frontend.js', __FILE__ ),
-        array(),
-        $asset_file['version'],
-        true
-    );
-
-    // Register block editor style
-    wp_register_style(
-        'quoteframeshare-block-editor',
-        plugins_url( 'build/index.css', __FILE__ ),
-        array(),
-        $asset_file['version']
-    );
-
-    // Register Font Awesome (used for social icons)
-    wp_register_style(
-        'quoteframeshare-fontawesome',
-        plugins_url( 'assets/fontawesome/all.min.css', __FILE__ ),
-        array(),
-        '6.5.1'
-    );
-
-    // Register frontend style, dependent on Font Awesome
-    wp_register_style(
-        'quoteframeshare-block-frontend',
-        plugins_url( 'build/frontend.css', __FILE__ ),
-        array( 'quoteframeshare-fontawesome' ),
-        '1.0.0'
-    );
-
-    // Register the block type
-    // register_block_type( 'quoteframeshare/block', array(
-    //     'editor_script' => 'quoteframeshare-block',
-    //     'editor_style'  => 'quoteframeshare-block-editor',
-    //     'style'         => 'quoteframeshare-block-frontend',
-    //     'script'        => 'quoteframeshare-frontend',
-    // ) );
-}
-// add_action( 'init', 'quoteframeshare_register_block' );
 
 /**
  * Enqueue Font Awesome for both frontend and editor.
  */
 function quoteframeshare_enqueue_fontawesome() {
-    wp_enqueue_style(
-        'quoteframeshare-fontawesome',
-        plugins_url( 'assets/fontawesome/all.min.css', __FILE__ ),
-        array(),
-        '6.7.2'
-    );
+	wp_enqueue_style(
+		'quoteframeshare-fontawesome',
+		plugins_url( 'assets/fontawesome/all.min.css', __FILE__ ),
+		array(),
+		'6.7.2'
+	);
 }
 add_action( 'wp_enqueue_scripts', 'quoteframeshare_enqueue_fontawesome' );
 add_action( 'enqueue_block_editor_assets', 'quoteframeshare_enqueue_fontawesome' );
@@ -91,14 +32,24 @@ add_action( 'enqueue_block_editor_assets', 'quoteframeshare_enqueue_fontawesome'
 /**
  * Add 'View details' link in the plugin row on the plugins page.
  */
-add_filter( 'plugin_row_meta', function( $links, $file ) {
-    if ( strpos( $file, 'quoteframeshare.php' ) !== false ) {
-        $links[] = '<a href="https://wordpress.org/plugins/quoteframeshare-blockquote-share-copy/" target="_blank">View details</a>';
-    }
-    return $links;
-}, 10, 2 );
+add_filter(
+	'plugin_row_meta',
+	function ( $links, $file ) {
+		if ( strpos( $file, 'quoteframeshare.php' ) !== false ) {
+			$links[] = '<a href="https://wordpress.org/plugins/quoteframeshare-blockquote-share-copy/" target="_blank">View details</a>';
+		}
+		return $links;
+	},
+	10,
+	2
+);
 
-function qs_block_init() {
+/**
+ * Register the QuoteFrameShare block.
+ *
+ * @return void
+ */
+function quoteframeshare_register_block() {
 	/**
 	 * Registers the block(s) metadata from the `blocks-manifest.php` and registers the block type(s)
 	 * based on the registered block metadata.
@@ -130,4 +81,4 @@ function qs_block_init() {
 		register_block_type( __DIR__ . "/build/{$block_type}" );
 	}
 }
-add_action( 'init', 'qs_block_init' );
+add_action( 'init', 'quoteframeshare_register_block' );
