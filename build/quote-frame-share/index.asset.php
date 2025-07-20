53e6829fbfb98cbc652ccad0ef958eb467879a11
Bunty <bn.bhandari90@gmail.com>
2025-07-20 22:05:31 +0530

    Add src folder for block and fix attempt recovery on page load

--- build/quote-frame-share/index.asset.php
+++ build/quote-frame-share/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '93e1eda808bb48df28a2');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => 'd829e9edbd387ab1f873');
